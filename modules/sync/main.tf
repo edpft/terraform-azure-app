1127589c0dab3c2ebbd1815e186eab60882fc4cc
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2023-05-21 22:33:46 +0100

    add service principle to sync

--- modules/sync/main.tf
+++ modules/sync/main.tf
@@ -1,3 +1,7 @@
 resource "azuread_application" "sync" {
   display_name = var.display_name
 }
+
+resource "azuread_service_principal" "sync" {
+  application_id = azuread_application.sync.application_id
+}
